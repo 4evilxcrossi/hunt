dfd35856a49019998239fd77bf94abb4bc5252f9
4evilxcrossi <98977464+4evilxcrossi@users.noreply.github.com>
2022-02-03 23:42:58 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash 
+apt-get install nmap
+pip install lolcat
+apt-get install  wapiti 
+cp astra /usr/local/bin
+chmod +x astra 
+chmod +X astra
+echo "if you were not as root please rerun setup.sh as root so you can call astra fom any terminal" | lolcat
+
