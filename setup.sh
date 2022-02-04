511467dc13f0aec74520283643103a76ef681bd0
crossi <98977464+4evilxcrossi@users.noreply.github.com>
2022-02-04 09:53:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash 
-apt-get install nmap
-pip install lolcat
-apt-get install  wapiti 
-cp astra /usr/local/bin
-chmod +x astra 
-chmod +X astra
+apt-get install nmap | lolcat
+pip install lolcat |lolcat
+apt-get install tor| lolcat
+apt-get install  wapiti| lolcat 
+cp astra /usr/local/bin| lolcat
+chmod +x /usr/local/bin/astra
 echo "if you were not as root please rerun setup.sh as root so you can call astra fom any terminal" | lolcat
-
